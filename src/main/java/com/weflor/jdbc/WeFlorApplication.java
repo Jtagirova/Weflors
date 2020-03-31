15acd83e0c9e17aaa9ccf1bb02148c80e83f91cf
Anna-Ivanchikova <Anna.Ivanchikova@ibm.com>
2020-03-31 11:50:10 +0300

    init repository

---
+++ src/main/java/com/weflor/jdbc/WeFlorApplication.java
@@ -0,0 +1,13 @@
+package com.weflor.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeFlorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeFlorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weflor/jdbc/WeFlorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weflor.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeFlorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
