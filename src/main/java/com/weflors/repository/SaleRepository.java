edbb46e16be2861d39d3a765fc77189c0e83933c
Atagiro2 <Atagiro2@ru.ibm.com>
2020-07-14 16:45:31 +0300

    Report test fix

--- src/main/java/com/weflors/repository/SaleRepository.java
+++ src/main/java/com/weflors/repository/SaleRepository.java
@@ -15,13 +15,13 @@ import java.util.List;
 
 public interface SaleRepository extends JpaRepository<SaleEntity, SaleEntityPK> {
 
-    @Query("select sale from SaleEntity sale where sale.productId = :productID")
+    @Query("select sale from SaleEntity sale where sale.productId = :productID and sale.salePrice <> 0")
     List<SaleEntity> findAllSalesByProductID(@Param("productID") Integer productID);
 
     @Query("select sale from SaleEntity sale where sale.saleDate >= :date")
     List<SaleEntity> findAllSalesByDate(@Param("date") Timestamp date);
 
-    @Query("select sale from SaleEntity sale where sale.productId = :productID and sale.saleDate >= :startDatePeriod and sale.saleDate <= :endDatePeriod")
+    @Query("select sale from SaleEntity sale where sale.productId = :productID and sale.saleDate >= :startDatePeriod and sale.saleDate <= :endDatePeriod and sale.salePrice <> 0")
     List<SaleEntity> findAllSalesByProductIDAndDatePeriod(
             @Param("productID") Integer productID, @Param("startDatePeriod") Timestamp startDatePeriod, @Param("endDatePeriod") Timestamp endDatePeriod);
     
